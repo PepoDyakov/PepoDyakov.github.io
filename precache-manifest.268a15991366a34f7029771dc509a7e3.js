93166a6dbea281a0a5fc7eb14e1307b6e19865bd
Pepo Dyakov <petar.dyakov@icloud.com>
2019-03-31 18:54:05 +0300

    Updates

--- precache-manifest.268a15991366a34f7029771dc509a7e3.js
+++ precache-manifest.268a15991366a34f7029771dc509a7e3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "91cd4b90dbb3560b2e66",
-    "url": "/static/js/main.9e199100.chunk.js"
+    "revision": "64fa45978f921465de04",
+    "url": "/static/js/main.7a937af9.chunk.js"
   },
   {
     "revision": "26d5f0c2abce3e11527d",
     "url": "/static/js/2.bbc56965.chunk.js"
   },
   {
-    "revision": "91cd4b90dbb3560b2e66",
-    "url": "/static/css/main.da47b29b.chunk.css"
+    "revision": "64fa45978f921465de04",
+    "url": "/static/css/main.55f30ab6.chunk.css"
   },
   {
-    "revision": "011e8a254745cb66d2124adc607c4a7a",
+    "revision": "64a29b22f08e7e185efed957c8be75ba",
     "url": "/index.html"
   }
 ];
