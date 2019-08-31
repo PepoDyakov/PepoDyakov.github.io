2718a42ea861e83a0dd38a4df7b36d1b3c66ebb3
PepoDyakov <petar.dyakov@icloud.com>
2019-08-31 15:14:23 +0300

    Updates

---
+++ precache-manifest.88bec176c4a68cbbf254cd5d9cdcde14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d08bf01f4a81b04cd565f3012684d95",
+    "url": "/static/media/logo.2d08bf01.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "95510e0f9f56369afc44",
+    "url": "/static/js/main.483312a1.chunk.js"
+  },
+  {
+    "revision": "fd55fb93a861acff3e34",
+    "url": "/static/js/2.71f8cd70.chunk.js"
+  },
+  {
+    "revision": "95510e0f9f56369afc44",
+    "url": "/static/css/main.2ce818fc.chunk.css"
+  },
+  {
+    "revision": "5fbbe216f8932cde8281f0d0697dd6d6",
+    "url": "/index.html"
+  }
+];
